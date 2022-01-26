88d06015e700f1cc92d095e7cf574b922bb945d6
Dylan Jacob <business.dmjacob@gmail.com>
2022-01-26 11:00:25 -0500

    messing around with template literals

--- app.js
+++ app.js
@@ -19,21 +19,42 @@
 // human.walk();
 
 
-function displayInfo() {
-        const nameEl = document.querySelector("#name");
-        const emailEl = document.querySelector("#email");
-        const buttonEl = document.querySelector("button");
+// function displayInfo() {
+//         const nameEl = document.querySelector("#name");
+//         const emailEl = document.querySelector("#email");
+//         const buttonEl = document.querySelector("button");
 
-        buttonEl.addEventListener("click", function() {
-            let name = nameEl.value;
-            let email = emailEl.value;
-            let result = {
-                name,
-                email
-            }
-            console.log(result)
-        })
+//         buttonEl.addEventListener("click", function() {
+//             let name = nameEl.value;
+//             let email = emailEl.value;
+//             let result = {
+//                 name,
+//                 email
+//             }
+//             console.log(result)
+//         })
+// }
+
+// displayInfo();
+
+function display(name, age, profession) {
+    console.log(`My name is ${name}. I am ${age} years old. I work as a ${profession}.`)
 }
 
-displayInfo();
+function createLongHTML() {
+    // let html = "<div>";
+    // html += "<p> hi this is paragraph </p>"
+    // html += "<h1> this is heading </h1>"
+    // html += "<div>"
 
+    let html = `
+        <div>
+            <p> hi this is a paragraph </p>
+            <h1> this is some heading </h1>
+        </div>
+    `
+    document.querySelector("body").innerHTML = html;
+
+}
+
+createLongHTML();
