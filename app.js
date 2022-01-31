d9f443ba74eb1797d84b0ca828b3aa3d65871503
Dylan Jacob <business.dmjacob@gmail.com>
2022-01-31 07:29:26 -0500

    more practicing with inheritance

--- app.js
+++ app.js
@@ -10,14 +10,28 @@ Person.prototype.greet = function() {
 
 // Programmer Constructor
 function Programmer(name, age, language) {
-    this.name = name;
-    this.age = age;
+    // this.name = name;
+    // this.age = age;
+    Person.call(this, name, age);
     this.language = language;
 }
 
+Programmer.prototype = Object.create(Person.prototype);
+
+const a = new Programmer("John", 22, "Java");
+const b = new Programmer("Naruto", 32, "Python")
+console.log(b.greet())
+
 // Fighter Constructor
 function Fighter(name, age, art) {
-    this.name = name;
-    this.age = age;
+    Person.call(this, name, age);
     this.art = art;
 }
+
+// always make sure to have these two lines of code when inheriting from a parent constructor
+Fighter.prototype = Object.create(Person.prototype);
+Fighter.prototype.constructor = Fighter;
+
+const johnny = new Fighter("Johnny", 56, "Eagle Fang")
+console.log(johnny.greet());
+console.log(johnny.constructor);
