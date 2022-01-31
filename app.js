94931d669ad4d6bde42a17037b61cfa49eafc391
Dylan Jacob <business.dmjacob@gmail.com>
2022-01-31 08:24:47 -0500

    es6 vs es5 constructor functions

--- app.js
+++ app.js
@@ -1,51 +1,29 @@
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
+// es6
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        return `My name is ${this.name}. I'm ${this.age} years old!`
+    }
 }
 
-Person.prototype.greet = function() {
-    return `My name is ${this.name}. I'm ${this.age} years old!`;
-}
+const instance = new Person("sam", 24)
 
 
-// Programmer Constructor
-function Programmer(name, age, language) {
-    Person.call(this, name, age);
-    this.language = language;
-}
+// // es5
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
 
-Programmer.prototype = Object.create(Person.prototype);
-Programmer.prototype.constructor = Programmer;
+// Person.prototype.greet = function() {
+//     return `My name is ${this.name}. I'm ${this.age} years old!` 
+// }
 
-// how to override methods
-Programmer.prototype.greet = function() {
-    return `${Person.prototype.greet.call(this)} I code in ${this.language}.`
-}
+// const instance = new Person("sam", 24)
 
-const a = new Programmer("Naruto", 32, "Java");
-console.log(a.greet())
-
-
-
-
-
-
-// Fighter Constructor
-function Fighter(name, age, art) {
-    Person.call(this, name, age);
-    this.art = art;
-}
-
-// always make sure to have these two lines of code when inheriting from a parent constructor
-Fighter.prototype = Object.create(Person.prototype);
-Fighter.prototype.constructor = Fighter;
-
-const johnny = new Fighter("Johnny", 56, "Eagle Fang")
-console.log(johnny.greet());
-console.log(johnny.constructor);
-
-Fighter.prototype.greet = function() {
-    return `${Person.prototype.greet.call(this)}. And I fight in ${this.art}!!!`
-}
-
-console.log(johnny.greet())
+// instance.name // sam
+// instance.age // 24
