3926413e5c28d1584ffea7289d2c66492cf8acf7
Dylan Jacob <business.dmjacob@gmail.com>
2022-01-31 07:11:38 -0500

    practicing inheritance

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-// this is a constrcutor function
-// you cannot have a return inside of a constructor function
-function Car(brand, model) {
-    this.brand = brand;
-    this.model = model;
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
 }
 
-// Car.prototype.stats = function() {
-//     return `${this.brand} ${this.model}`;
-// }
-
-Car.prototype.drive = function() {
-    return `I'm driving a ${this.brand} ${this.model} vrooooom`;
+Person.prototype.greet = function() {
+    return `My name is ${this.name}. I'm ${this.age} years old!`;
 }
 
-let car1 = new Car("lambo", "aventador");
-let car2 = new Car ("toyota", "camry");
-let car3 = new Car ("dodge", "charger");
 
-console.log(car1.drive());
-console.log(car2.drive());
-console.log(car3.drive());
+// Programmer Constructor
+function Programmer(name, age, language) {
+    this.name = name;
+    this.age = age;
+    this.language = language;
+}
+
+// Fighter Constructor
+function Fighter(name, age, art) {
+    this.name = name;
+    this.age = age;
+    this.art = art;
+}
