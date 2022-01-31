000cd81d1d8c688bf3661082e2cbe57df5f87488
Dylan Jacob <business.dmjacob@gmail.com>
2022-01-31 08:36:50 -0500

    method overriding in es6

--- app.js
+++ app.js
@@ -10,20 +10,42 @@ class Person {
     }
 }
 
-const instance = new Person("sam", 24)
+const a = new Person("sam", 24)
 
+// ---------------------------------------
 
-// // es5
-// function Person(name, age) {
-//     this.name = name;
-//     this.age = age;
-// }
+class Programmer extends Person {
+    constructor(name, age, language) {
+        super(name, age);
+        this.language = language;
+    }
 
-// Person.prototype.greet = function() {
-//     return `My name is ${this.name}. I'm ${this.age} years old!` 
-// }
+    greet() {
+        return `${super.greet()}. I code in ${this.language}. `
+    }
 
-// const instance = new Person("sam", 24)
+    talk() {
+        return "I'm just talking"
+    }
+}
 
-// instance.name // sam
-// instance.age // 24
+const b = new Programmer("Julie", 22, "c")
+
+console.log(b.greet())
+
+// ---------------------------------------
+
+class Fighter extends Person {
+    constructor(name, age, art) {
+        super(name, age);
+        this.art = art;
+    }
+
+    fight() {
+        return "I'm fighting!!!"
+    }
+
+}
+
+const c = new Fighter("Naruto", 32, "talk no jutsu");
+console.log(c.fight())
