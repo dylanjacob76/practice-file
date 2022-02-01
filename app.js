10f1890094b75bf6fb1326aea1413c936d643e50
Dylan Jacob <business.dmjacob@gmail.com>
2022-02-01 11:30:48 -0500

    practicing passing by value and by reference. Also some memory allocation

--- app.js
+++ app.js
@@ -1,51 +1,16 @@
-// es6
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
+let name = "Tom";
+let nameCopy = name;
 
-    greet() {
-        return `My name is ${this.name}. I'm ${this.age} years old!`
-    }
-}
+name += " Hardy";
 
-const a = new Person("sam", 24)
+console.log(name)
+console.log(nameCopy);
 
-// ---------------------------------------
 
-class Programmer extends Person {
-    constructor(name, age, language) {
-        super(name, age);
-        this.language = language;
-    }
+let fruits = ["cherry", "mango"];
+let fruitsCopy = fruits;
 
-    greet() {
-        return `${super.greet()}. I code in ${this.language}. `
-    }
+fruits.push("apple", "bananna")
 
-    talk() {
-        return "I'm just talking"
-    }
-}
-
-const b = new Programmer("Julie", 22, "c")
-
-console.log(b.greet())
-
-// ---------------------------------------
-
-class Fighter extends Person {
-    constructor(name, age, art) {
-        super(name, age);
-        this.art = art;
-    }
-
-    fight() {
-        return "I'm fighting!!!"
-    }
-
-}
-
-const c = new Fighter("Naruto", 32, "talk no jutsu");
-console.log(c.fight())
+console.log(fruits);
+console.log(fruitsCopy);
