9ccac761202701a81d94ebaa4bb3ad1856052ec8
Dylan Jacob <business.dmjacob@gmail.com>
2022-02-09 10:32:01 -0500

    messing around with closures

--- app.js
+++ app.js
@@ -1,23 +1,59 @@
-// DECLARATIONS
-function alpha() {
-    console.log("alpha speaking!")
+// any function that's using variables outside of it's own scope is a closure
+
+let a = 6; //global scope
+
+let product = function() {
+    let b = 7;
+    return a * b; //this is a closure because we're using a which is a variable declared in the global scope
 }
 
-alpha();
+console.log(product());
 
-function alpha() {
-    console.log("new alpha speaking")
+// ----------------------------------------------------------------
+
+// a closure is an inner function that uses it's outer function's variables
+
+function outer() {
+    let varA = "HI"; //declared outside the inner function/block
+    function inner() {
+        return varA + " Bye"; //varA is declared outside of this scope therefore this is a closure
+    }
+    return inner;
 }
 
 
-// EXPRESSIONS
-var alpha = function() {
-    console.log("alpha speaking")
+let myFunc = outer();
+
+console.log(myFunc());
+
+// ----------------------------------------------------------------
+
+function greeting(type) {
+    return function(name) {
+        console.log(`${type}, ${name}!!!`);
+    }
 }
 
-alpha();
+const sayHello = greeting("Hello")
+const sayHi = greeting("Hi");
+const sayBonjour = greeting("Bonjour");
 
-var alpha = function() {
-    console.log("new alpha speaking");
+sayHello("Mike");
+sayHi("John");
+sayBonjour("Thomas");
+
+// ----------------------------------------------------------------
+
+function html(tag) {
+    return function(innerValue) {
+        return `<${tag}>${innerValue}</${tag}>`
+    }
 }
 
+const h1 = html("h1");
+const p = html("p");
+const button = html("button");
+
+console.log(h1("this is a heading 1"))
+console.log(p("this is a paragraph"))
+console.log(button("button"))
