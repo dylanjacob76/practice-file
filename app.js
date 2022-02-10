b93aaedc9c32c65ea2e9b42240125bc5e5d65dfc
Dylan Jacob <business.dmjacob@gmail.com>
2022-02-10 14:24:01 -0500

    messing around with callbacks

--- app.js
+++ app.js
@@ -1,81 +1,58 @@
-// any function that's using variables outside of it's own scope is a closure
+// a callback function is a function that is passed into another function as an argument...
+// this "passed-in " function is invoked in the body of the other function
 
-// let a = 6; //global scope
-
-// let product = function() {
-//     let b = 7;
-//     return a * b; //this is a closure because we're using a which is a variable declared in the global scope
-// }
-
-// console.log(product());
-
-// ----------------------------------------------------------------
-
-// a closure is an inner function that uses it's outer function's variables
-
-// function outer() {
-//     let varA = "HI"; // outer functions variable
-//     function inner() { // inner function
-//         return varA + " Bye"; 
-//     }
-//     return inner;
-// }
-
-// let myFunc = outer();
-
-// console.log(myFunc());
-
-// ---------------------------------------------------------------- 
-
-// function greeting(type) {
-//     return function(name) {
-//         console.log(`${type}, ${name}!!!`)
-//     }
-// }
-
-// const sayHello = greeting("hello");
-// const sayHi = greeting("hi");
-// const sayWhatUp = greeting("what's up");
-
-// sayHello("Ichigo");
-// sayHi("Luffy");
-// sayWhatUp("Naruto");
-
-// ---------------------------------------------------------------- 
-
-// function html(tag) {
-//     return function(innerValue) {
-//         return `<${tag}>${innerValue}</${tag}>`
-//     }
-// }
-
-// const h1Tag = html("h1");
-// const buttonTag = html("button");
-
-// console.log(h1Tag("This is a header 1"))
-// console.log(h1Tag("This is a header 2"))
-// console.log(h1Tag("This is a header 3"))
-// console.log(buttonTag("on button"))
-// console.log(buttonTag("off button"))
-
-// ---------------------------------------------------------------- 
-
-function nameModule() {
-    let name = "default"; // private
-
-    return {
-        getName: function() {
-            return name;
-        },
-        setName: function(newName) {
-            name = newName;
-        }
+const squareZap = (arr) => {
+    let result = [];
+    for(let item of arr) {
+        result.push(Math.pow(item, 2));
     }
+    return result;
+}
+console.log(squareZap([1, 2, 3]))
+
+const doubleZap = (arr) => {
+    let result = [];
+    for (let item of arr) {
+        result.push(item * 2)
+    }
+    return result;
+}
+console.log(doubleZap([1, 2, 3]))
+
+const squareRootZap = (arr) => {
+    let result = [];
+    for (let item of arr) {
+        result.push(Math.sqrt(item));
+    }
+    return result;
+}
+console.log(squareRootZap([1, 2, 3]))
+
+const upperCaseZap = (arr) => {
+    let result = [];
+    for (let item of arr) {
+        result.push(item.toUpperCase());
+    }
+    return result;
+}
+console.log(upperCaseZap(["what up"]))
+
+
+
+// using callback
+
+const masterZap = (arr, callback) => {
+    let result = [];
+    for(let item of arr) {
+        result.push(callback(item));
+    }
+    return result;
 }
 
-// closures store references to the outer function's variables
+const uppercase = (item) => item.toUpperCase();
 
-const myName = nameModule(); // storing the reference to the name variable
-console.log(myName.getName());
-myName.setName("Naruto");
-console.log(myName.getName());
+const double = (item) => item * 2;
+
+const square = (item) => Math.pow(item, 2);
+
+console.log(masterZap([1, 2, 3], double));
