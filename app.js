c44efcbe11a5a8e426c5aeec64917e6803fbb826
Dylan Jacob <business.dmjacob@gmail.com>
2022-02-10 09:41:22 -0500

    reviewing more closure examples

--- app.js
+++ app.js
@@ -1,59 +1,81 @@
 // any function that's using variables outside of it's own scope is a closure
 
-let a = 6; //global scope
+// let a = 6; //global scope
 
-let product = function() {
-    let b = 7;
-    return a * b; //this is a closure because we're using a which is a variable declared in the global scope
-}
+// let product = function() {
+//     let b = 7;
+//     return a * b; //this is a closure because we're using a which is a variable declared in the global scope
+// }
 
-console.log(product());
+// console.log(product());
 
 // ----------------------------------------------------------------
 
 // a closure is an inner function that uses it's outer function's variables
 
-function outer() {
-    let varA = "HI"; //declared outside the inner function/block
-    function inner() {
-        return varA + " Bye"; //varA is declared outside of this scope therefore this is a closure
+// function outer() {
+//     let varA = "HI"; // outer functions variable
+//     function inner() { // inner function
+//         return varA + " Bye"; 
+//     }
+//     return inner;
+// }
+
+// let myFunc = outer();
+
+// console.log(myFunc());
+
+// ---------------------------------------------------------------- 
+
+// function greeting(type) {
+//     return function(name) {
+//         console.log(`${type}, ${name}!!!`)
+//     }
+// }
+
+// const sayHello = greeting("hello");
+// const sayHi = greeting("hi");
+// const sayWhatUp = greeting("what's up");
+
+// sayHello("Ichigo");
+// sayHi("Luffy");
+// sayWhatUp("Naruto");
+
+// ---------------------------------------------------------------- 
+
+// function html(tag) {
+//     return function(innerValue) {
+//         return `<${tag}>${innerValue}</${tag}>`
+//     }
+// }
+
+// const h1Tag = html("h1");
+// const buttonTag = html("button");
+
+// console.log(h1Tag("This is a header 1"))
+// console.log(h1Tag("This is a header 2"))
+// console.log(h1Tag("This is a header 3"))
+// console.log(buttonTag("on button"))
+// console.log(buttonTag("off button"))
+
+// ---------------------------------------------------------------- 
+
+function nameModule() {
+    let name = "default"; // private
+
+    return {
+        getName: function() {
+            return name;
+        },
+        setName: function(newName) {
+            name = newName;
+        }
     }
-    return inner;
 }
 
+// closures store references to the outer function's variables
 
-let myFunc = outer();
-
-console.log(myFunc());
-
-// ----------------------------------------------------------------
-
-function greeting(type) {
-    return function(name) {
-        console.log(`${type}, ${name}!!!`);
-    }
-}
-
-const sayHello = greeting("Hello")
-const sayHi = greeting("Hi");
-const sayBonjour = greeting("Bonjour");
-
-sayHello("Mike");
-sayHi("John");
-sayBonjour("Thomas");
-
-// ----------------------------------------------------------------
-
-function html(tag) {
-    return function(innerValue) {
-        return `<${tag}>${innerValue}</${tag}>`
-    }
-}
-
-const h1 = html("h1");
-const p = html("p");
-const button = html("button");
-
-console.log(h1("this is a heading 1"))
-console.log(p("this is a paragraph"))
-console.log(button("button"))
+const myName = nameModule(); // storing the reference to the name variable
+console.log(myName.getName());
+myName.setName("Naruto");
+console.log(myName.getName());
